5784319180ecfc405f86c1d972c137e1fe416000
rk3333333 <144227727+rk3333333@users.noreply.github.com>
2024-04-22 18:46:43 +0200

    ik that the code sucks but IDC

--- Ripterms/Modules/LeftClicker.cpp
+++ Ripterms/Modules/LeftClicker.cpp
@@ -3,21 +3,53 @@
 #include "../Cache/Cache.h"
 #include <ImGui/imgui.h>
 
-void Ripterms::Modules::LeftClicker::run()
+void Ripterms::Modules::LeftClicker::LC()
 {
-	static CTimer timer(std::chrono::milliseconds(1000/min_cps));
-	if (!enabled || Ripterms::GUI::draw || !(GetKeyState(VK_LBUTTON) & 0x8000) || cache->theMinecraft.getCurrentScreen().isValid())
-		return;
-	if (!timer.isElapsed()) 
-		return;
-	if (Ripterms::cache->theMinecraft.getObjectMouseOver().getType().isEqualTo(MovingObjectType::getType("BLOCK")))
-		return;
+	static CTimer timer(std::chrono::milliseconds(1000 / min_cps));
 	POINT cursorPos{};
 	GetCursorPos(&cursorPos);
 	PostMessageA(Ripterms::window, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(cursorPos.x, cursorPos.y));
 	PostMessageA(Ripterms::window, WM_LBUTTONUP, MK_LBUTTON, MAKELPARAM(cursorPos.x, cursorPos.y));
 	std::uniform_int_distribution<> distr(min_cps, max_cps);
 	timer.setEvery(std::chrono::milliseconds(1000 / distr(gen)));
+}
+
+void Ripterms::Modules::LeftClicker::run()
+{
+	static CTimer timer(std::chrono::milliseconds(1000/min_cps));
+	if (!enabled || Ripterms::GUI::draw)
+		return;
+	if (!timer.isElapsed()) 
+		return;
+	if (Ripterms::cache->theMinecraft.getObjectMouseOver().getType().isEqualTo(MovingObjectType::getType("BLOCK")))
+		return;
+
+	if (cache->theMinecraft.getCurrentScreen().isValid()) {
+		if (in_gui)
+		{
+			if (hold_mb)
+			{
+				if ((GetKeyState(VK_LBUTTON) & 0x8000))
+				{
+					LC();
+				}
+			}
+			else {
+				LC();
+			}
+		}
+	}else {
+		if (hold_mb)
+		{
+			if ((GetKeyState(VK_LBUTTON) & 0x8000))
+			{
+				LC();
+			}
+		}
+		else {
+			LC();
+		}
+	}
 }
 
 void Ripterms::Modules::LeftClicker::renderGUI()
@@ -27,6 +59,8 @@ void Ripterms::Modules::LeftClicker::renderGUI()
 	{
 		ImGui::SetCursorPosX(ImGui::GetCursorPosX() + 15.0f);
 		ImGui::BeginGroup();
+		ImGui::Checkbox("Hold mb", &hold_mb);
+		ImGui::Checkbox("In GUI", &in_gui);
 		ImGui::SliderInt("Min CPS", &min_cps, 1, 150);
 		ImGui::SliderInt("Max CPS", &max_cps, 1, 150);
 		ImGui::EndGroup();
