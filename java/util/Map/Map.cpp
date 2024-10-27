26ded466452265eed27fbe2e4ba603ec7b59a614
rk3333333 <144227727+rk3333333@users.noreply.github.com>
2024-10-27 21:37:55 +0100

    Delete java directory

--- java/util/Map/Map.cpp
+++
@@ -1,7 +0,0 @@
-#include "Map.h"
-
-Object Map::put(const Object& key, const Object& value)
-{
-	if (!instance) return Object();
-	return Object(env->CallObjectMethod(instance, MapClass.getMethodID("put"), key.getInstance(), value.getInstance()), env);
-}
